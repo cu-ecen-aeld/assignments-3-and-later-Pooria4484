d8c54bb0d9842ef3e0ee4b99f9bdcab821b6e83c
Pooria4484 <47162303+Pooria4484@users.noreply.github.com>
2025-03-20 22:05:04 +0330

    Update finder-test.sh for assignmnet-4-p2

--- finder-app/finder-test.sh
+++ finder-app/finder-test.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
-# Tester script for assignment 1 and assignment 2
+# Tester script for assignment 1 and assignment 2, modified for Assignment 4
 # Author: Siddhant Jajoo
 # Modified for Assignment 2 to use native-compiled writer application
+# Modified for Assignment 4 to use PATH, /etc/finder-app/conf/, and write output to /tmp/assignment4-result.txt
 
 set -e
 set -u
@@ -9,7 +10,7 @@ set -u
 NUMFILES=10
 WRITESTR=AELD_IS_FUN
 WRITEDIR=/tmp/aeld-data
-username=$(cat conf/username.txt)
+username=$(cat /etc/finder-app/conf/username.txt)
 
 if [ $# -lt 3 ]
 then
@@ -33,7 +34,7 @@ echo "Writing ${NUMFILES} files containing string ${WRITESTR} to ${WRITEDIR}"
 rm -rf "${WRITEDIR}"
 
 # create $WRITEDIR if not assignment1
-assignment=$(cat ../conf/assignment.txt)
+assignment=$(cat /etc/finder-app/conf/assignment.txt)
 
 if [ "$assignment" != 'assignment1' ]
 then
@@ -49,19 +50,15 @@ then
     fi
 fi
 
-# Clean previous build artifacts and compile writer as native application
-echo "Cleaning previous build artifacts and compiling writer application"
-# make clean
-# make
-
 for i in $(seq 1 "$NUMFILES")
 do
-    ./writer "$WRITEDIR/${username}$i.txt" "$WRITESTR"
+    writer "$WRITEDIR/${username}$i.txt" "$WRITESTR"
 done
 
-echo $(pwd)
+OUTPUTSTRING=$(finder.sh "$WRITEDIR" "$WRITESTR")
 
-OUTPUTSTRING=$(/home/finder.sh "$WRITEDIR" "$WRITESTR")
+# Write finder output to /tmp/assignment4-result.txt
+echo "$OUTPUTSTRING" > /tmp/assignment4-result.txt
 
 # remove temporary directories
 rm -rf /tmp/aeld-data
